9b7ccbb8d28e0f4857760c3aca6713d26ad0e7f5
pscube <sambhav@solutions1313.com>
2022-10-21 13:23:17 +0530

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
+      theme: ThemeData(fontFamily: 'Inter'),
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
