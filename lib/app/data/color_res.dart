1341a39c7eaf2ab124614d392398bc4d9dff84b9
pscube <sambhav@solutions1313.com>
2022-10-21 12:35:43 +0530

    basic setup completed

---
+++ lib/app/data/color_res.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorRes{
+
+  static Color primaryTextColor = Colors.white;
+  static Color secondaryTextColor = const Color(0xffD9D9D9);
+  static Color backgroundColor = const Color(0xff070818);
+  static Color brandColor = const Color(0xff007CFF);
+  static Color secondaryColor = const Color(0xff252634);
+
+}
